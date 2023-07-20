85d64fbe31d53d41f9ebf19caf5a20ad13f42ac0
artemtret <artem.tretykov74@gmail.com>
2023-07-20 15:36:48 +0300

    starting 3 handler

--- app/handlers/user_interaction.py
+++ app/handlers/user_interaction.py
@@ -45,7 +45,63 @@ async def start_cmd(message: types.Message):
 @dp.message_handler(Text(equals="🚀 Начать"), state=UserFollowing.start_navigation)
 async def request_wallets(message: types.Message):
     await UserFollowing.ask_wallet.set()
-    await message.answer("👝 *Отправьте свой кошелек* \n"
-                         "(если вы хотите отправить несколько кошельков, то разделите кошельки построчно)",
+    await message.answer("👝 *Отправьте свой кошелек* \n\n"
+                         "_Если Вы хотите отправить несколько кошельков, то разделите кошельки построчно_",
                          parse_mode=types.ParseMode.MARKDOWN,
                          reply_markup=ReplyKeyboardRemove())
+
+
+@dp.message_handler(state=UserFollowing.ask_wallet)
+async def send_wallet_stats(message: types.Message):
+    user_wallets = message.text
+
+    wait_message = await message.answer("⏳ Получаю кошельки...", reply_markup=ReplyKeyboardRemove())
+
+    if '\n' in user_wallets:
+        lst_user_wallets = user_wallets.split('\n')
+        for i in range(len(lst_user_wallets)):
+            lst_user_wallets[i].strip()
+            if len(lst_user_wallets[i]) != 42:
+                error_message = f" Неверный формат (либо ошибка в получении кошелька #{i + 1})"
+
+                buttons = [
+                    KeyboardButton(text="Попробовать снова"),
+                ]
+                reply_markup = ReplyKeyboardMarkup(keyboard=[buttons],
+                                                   resize_keyboard=True)
+
+                await bot.delete_message(chat_id=wait_message.chat.id,
+                                         message_id=wait_message.message_id)
+                await message.answer(error_message, parse_mode=types.ParseMode.MARKDOWN,
+                                     reply_markup=reply_markup)
+                return
+        lst_user_wallets = list(set(lst_user_wallets))
+        await bot.delete_message(chat_id=wait_message.chat.id,
+                                 message_id=wait_message.message_id)
+        success_message = await message.answer(f" _Получено кошельков: {len(lst_user_wallets)}_",
+                                               parse_mode=types.ParseMode.MARKDOWN)
+    else:
+        user_wallet = user_wallets.strip()
+
+        if len(user_wallet) != 42:
+            error_message = f" Неверный формат кошелька"
+
+            buttons = [
+                KeyboardButton(text="Попробовать снова"),
+            ]
+            reply_markup = ReplyKeyboardMarkup(keyboard=[buttons],
+                                               resize_keyboard=True)
+
+            await bot.delete_message(chat_id=wait_message.chat.id,
+                                     message_id=wait_message.message_id)
+            await message.answer(error_message, parse_mode=types.ParseMode.MARKDOWN,
+                                 reply_markup=reply_markup)
+            return
+        await bot.delete_message(chat_id=wait_message.chat.id,
+                                 message_id=wait_message.message_id)
+        lst_user_wallets = [user_wallet]
+        success_message = await message.answer(f" _Кошелек успешно получен_", parse_mode=types.ParseMode.MARKDOWN)
+
+    wait_1_message = await message.answer("⏳ Готовлю статистику...")
+    response_message = ""
+
