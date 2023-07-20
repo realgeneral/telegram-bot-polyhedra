406882bc910b26be05a23f2b2f20cdfcd8b393c7
artemtret <artem.tretykov74@gmail.com>
2023-07-21 00:41:47 +0300

    config files

---
+++ app/utils/extra.py
@@ -0,0 +1,15 @@
+rpcs = {'bsc': 'https://rpc.ankr.com/bsc',
+        'polygon': 'https://polygon-rpc.com',
+        'core': 'https://rpc.coredao.org',
+        'opbnb': 'https://opbnb-testnet-rpc.bnbchain.org',
+        'celo': 'https://rpc.ankr.com/celo'}
+
+scanners = {
+            'bsc': 'https://bscscan.com',
+            'polygon': 'https://polygonscan.com',
+            'core': 'https://scan.coredao.org',
+            'opbnb': 'https://opbnbscan.com',
+            'celo': 'https://celoscan.io'
+            }
+
+API_KEY = ""
