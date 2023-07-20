de53a97f3bd71acb3d94a8c6c9bdc94b1f1d489b
artemtret <artem.tretykov74@gmail.com>
2023-07-21 00:42:42 +0300

    add new states

--- app/states/UserFollowing.py
+++ app/states/UserFollowing.py
@@ -4,5 +4,8 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 class UserFollowing(StatesGroup):
     start_navigation = State()
     ask_wallet = State()
+    wallet_menu = State()
+    claim_all = State()
+    send_API = State()
     send_statistics = State()
 
