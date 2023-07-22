99d0bcc30d4d423bfe7354e865fcc9934d83a7c5
artemtret <artem.tretykov74@gmail.com>
2023-07-22 14:32:39 +0300

    env variable BOT_TOKEN

--- app/create_bot.py
+++ app/create_bot.py
@@ -4,7 +4,7 @@ from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-BOT_TOKEN = ""
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 
 bot = Bot(token=BOT_TOKEN)
